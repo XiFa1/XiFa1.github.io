cda8250edd8b2e474f0cdf6c1cf82386fd2b4686
XiFa1 <2098582322@qq.com>
2026-01-15 19:22:11 +0800

    Site updated: 2026-01-15 19:22:10

--- js/custom.js
+++ js/custom.js
@@ -8,9 +8,9 @@ function showAnnouncement() {
     // <br> 是换行，<b> 是加粗，<div style="text-align:left"> 是为了让文字左对齐更像文章
     html: `
       <div style="text-align: left; line-height: 1.8;">
-        1. 欢迎访问我的博客！🎉<br>
-        2. 欢迎加入QQ交流群:1060729358。<br>
-        3. 遇到问题可以在留言板反馈。<br>
+        欢迎访问我的博客！🎉<br>
+        欢迎加入QQ交流群:1060729358。<br>
+        📢 评论区架设在海外，如无法加载请尝试开启魔法（VPN）~<br>
       </div>
     `,
     icon: 'info',           // 图标可选：success, error, warning, info, question
